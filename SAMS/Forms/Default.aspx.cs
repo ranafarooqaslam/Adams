fc4253bb701a0bad99e1464b58ac8e74041c2c25
Saqib <Saqib@SAQIB>
2026-03-03 15:28:17 +0500

    bottom space reduced

--- SAMS/Forms/Default.aspx.cs
+++ SAMS/Forms/Default.aspx.cs
@@ -3,33 +3,38 @@ using CrystalDecisions.CrystalReports.Engine;
 using System.IO;
 using SAMSCommon.Classes;
 using CrystalDecisions.Shared;
+using System.Web;
+using System.Threading;
+
 public partial class Forms_Default : System.Web.UI.Page
 {
     ReportDocument CrpReport = null;
     System.IO.Stream oStream = null;
     protected void Page_Load(object sender, EventArgs e)
     {
-        CrpReport = (ReportDocument)this.Session["CrpReport"];
-        byte[] byteArray = null;
+        ReportDocument CrpReport = (ReportDocument)this.Session["CrpReport"];
+        Stream oStream = null;
+
         try
         {
             if (int.Parse(this.Session["ReportType"].ToString()) == 0)
             {
-                oStream = (System.IO.Stream)CrpReport.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
-                byteArray = new byte[oStream.Length];
-                oStream.Read(byteArray, 0, Convert.ToInt32(oStream.Length - 1));
+                oStream = CrpReport.ExportToStream(ExportFormatType.PortableDocFormat);
+
+                byte[] byteArray = new byte[oStream.Length];
+                oStream.Read(byteArray, 0, Convert.ToInt32(oStream.Length));
+
                 Response.Clear();
-                Response.Buffer = true;
                 Response.ContentType = "application/pdf";
                 Response.BinaryWrite(byteArray);
-                Response.End();
+                Response.Flush();
             }
             else
             {
                 string path = SAMSCommon.Classes.Configuration.GetAppInstallationPath() + "\\Exported.xls";
-                CrpReport.SetDatabaseLogon("sa", "123");
                 CrpReport.ExportToDisk(ExportFormatType.Excel, path);
-                System.IO.FileInfo file = new System.IO.FileInfo(path);
+
+                FileInfo file = new FileInfo(path);
 
                 if (file.Exists)
                 {
@@ -38,34 +43,35 @@ public partial class Forms_Default : System.Web.UI.Page
                     Response.AddHeader("Content-Length", file.Length.ToString());
                     Response.ContentType = "application/octet-stream";
                     Response.WriteFile(file.FullName);
-                    Response.End();
-                }
-                else
-                {
-                    Response.Write("This file does not exist.");
+                    Response.Flush();
                 }
             }
-            CrpReport.Close();
-            CrpReport.Dispose();
-            this.Session.Remove("CrpReport");
-            oStream.Close();
-            oStream.Dispose();
-            GC.Collect();
+
+            HttpContext.Current.ApplicationInstance.CompleteRequest();
+        }
+        catch (ThreadAbortException)
+        {
+            // Ignore - caused by Response handling
         }
         catch (Exception exp)
         {
             ExceptionPublisher.PublishException(exp);
-
         }
         finally
         {
-            CrpReport.Close();
-            CrpReport.Dispose();
-            this.Session.Remove("CrpReport");
-            oStream.Flush();
-            oStream.Close();
-            oStream.Dispose();
-            GC.Collect();
+            if (CrpReport != null)
+            {
+                CrpReport.Close();
+                CrpReport.Dispose();
+            }
+
+            if (oStream != null)
+            {
+                oStream.Close();
+                oStream.Dispose();
+            }
+
+            Session.Remove("CrpReport");
         }
     }
 
